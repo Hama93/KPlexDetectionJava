c7a90f1d2e0d93841351af78de6884bea182f48e
Hama93 <149497095+Hama93@users.noreply.github.com>
2024-04-30 21:02:28 +0100

    Add files via upload

---
+++ KPlexDetection.java
@@ -0,0 +1,88 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package kplexdetection;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import kplexdetection.javaClassesAndInterfaces.Graph;
+import kplexdetection.javaClassesAndInterfaces.GraphMap;
+
+/**
+ *
+ * @author haythem
+ */
+public class KPlexDetection {
+
+    private static void detectKPlexRecursive(Graph g, int k, Set<String> candidate_set, List<Set<String>> listOfMaximals) {
+        if (g.isMaximalKPlex(candidate_set, k)) {
+            listOfMaximals.add(candidate_set);
+        } else {
+            Graph graph = g.getGraphInducedBy(candidate_set);
+            if (graph.isConnected() && (!graph.isKPlex(k))) {
+                for (String vertex : candidate_set) {
+                    Set<String> next_set = new HashSet<>(candidate_set);
+                    next_set.remove(vertex);
+                    detectKPlexRecursive(g, k, next_set, listOfMaximals);
+                }
+            }
+        }
+    }
+
+    private static List<Set<String>> detectKPlex(Graph g, int k) {
+        Set<String> vertices = g.getVertices();
+        List<Set<String>> listOfMaximals = new ArrayList<>();
+        detectKPlexRecursive(g, k, vertices, listOfMaximals);
+        return listOfMaximals;
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            List<String> lines = Files.readAllLines(Paths.get("src/kplexdetection/datas/example2.txt"));
+            Graph g = new GraphMap();
+            g.addEdges(lines);
+            int kmax = g.getMaxK();
+            List<Set<String>> ll = new ArrayList<>();
+            List<Set<String>> listOfMaximals;
+            int n;
+            int i;
+            Set<String> vertices;
+            for (int k = kmax; k >= 2; k--) {
+                n = ll.size();
+                i = 0;
+                listOfMaximals = new ArrayList<>();
+                long startTime = System.nanoTime();  // Start timing here
+                do{
+                    if(k == kmax){
+                        vertices = g.getVertices();
+                    }else{
+                        vertices = ll.get(i);
+                    }
+                    detectKPlexRecursive(g, k, vertices, listOfMaximals);
+                    i++;
+                } while (i < n);
+                ll = listOfMaximals;
+                long endTime = System.nanoTime();  // End timing here
+                //ll = detectKPlex(g, k);
+                double duration = (double) (endTime - startTime) / 1_000_000_000;  // Compute the duration in seconds
+                //System.out.println("List of detected " + k + "-plex :" + listOfMaximals);
+                System.out.println("Execution time for k = " + k + " : " + duration + " seconds");
+                System.out.println("Number of " + k + "-plex detected = " + ll.size());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
